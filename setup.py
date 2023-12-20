19dcb7592ffb95462d104cd6b81facfba8eebc4f
deadbeef-development <90785600+deadbeef-development@users.noreply.github.com>
2023-12-20 15:35:14 +0000

    Add setup.py and setup instructions

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Py-Certpin',
+    version='0.0.0',
+    py_modules=['certpin'],
+    author='deadbeef-development',
+    author_email='deadbeef.development@gmail.com'
+)
