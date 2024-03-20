cf1231bbbd5712dfcdced59b28d1b31d0563c9ce
deadbeef-development <90785600+deadbeef-development@users.noreply.github.com>
2024-03-20 19:54:35 +0000

    Add capture utility

---
+++ certpin/capture.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+from .util import open_ssl_connection
+
+parser = ArgumentParser()
+
+parser.add_argument('server_address', help='host:port')
+parser.add_argument('der_dest_file_path')
+
+parser.add_argument('--server-name', default=None)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    host, port = args.server_address.split(':')
+
+    ssl_server_addr = host, int(port)
+
+    with open_ssl_connection(ssl_server_addr, args.server_name) as sock:
+        with open(args.der_dest_file_path, 'wb') as fio:
+            der = sock.getpeercert(binary_form=True)
+            fio.write(der)
+
