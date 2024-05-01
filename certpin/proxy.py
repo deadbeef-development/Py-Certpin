4d01775fc85bb97c4f62f610d270b36536b63f69
deadbeef-development <90785600+deadbeef-development@users.noreply.github.com>
2024-05-01 23:08:55 +0000

    Update proxy

--- certpin/proxy.py
+++ certpin/proxy.py
@@ -20,17 +20,9 @@ class ProxyNotFound(Exception):
 class ProxyHandler(BaseRequestHandler):
     def handle(self):
         self.server: ProxyServer
-
-        client_connection = self.request
         
-        # Retrieve Proxy object based on SNI (server_name)
-        server_name = client_connection.server_hostname
-        proxy = self.server.get_proxy(server_name)
-        
-        # Setup the upstream socket
-        upstream_socket = proxy.connect_upstream()
-
-        bridge_sockets(client_connection, upstream_socket)
+        with self.request.proxy.connect_upstream() as upstream_socket:
+            bridge_sockets(self.request, upstream_socket)
 
 class ProxyServer(ThreadingMixIn, TCPServer):
     def __init__(self, address: Tuple[str, int], get_proxy: Callable[[str], Proxy]):
@@ -40,8 +32,8 @@ class ProxyServer(ThreadingMixIn, TCPServer):
         self.ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         self.ssl_context.sni_callback = self.sni_callback
 
-    def sni_callback(self, ssl_sock, server_name, initial_context):
-        proxy = self.get_proxy(server_name)
+    def sni_callback(self, ssl_sock: ssl.SSLSocket, server_name: str, initial_context: ssl.SSLContext):
+        proxy = ssl_sock.proxy = self.get_proxy(server_name)
 
         if proxy is None:
             raise ProxyNotFound(server_name)
@@ -49,7 +41,7 @@ class ProxyServer(ThreadingMixIn, TCPServer):
         ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         ssl_context.load_cert_chain(certfile=proxy.certfile, keyfile=proxy.keyfile)
         
-        return ssl_context
+        ssl_sock.context = ssl_context
 
     def get_request(self):
         newsocket, fromaddr = super().get_request()
