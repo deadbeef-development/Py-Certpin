d24de935cf7d1b3f731802f21a249ef4b710f3e8
deadbeef-development <90785600+deadbeef-development@users.noreply.github.com>
2024-05-01 22:26:50 +0000

    Rename parameter

--- certpin/capture.py
+++ certpin/capture.py
@@ -6,7 +6,7 @@ from .util import connect_ssl_insecure
 parser = ArgumentParser()
 
 parser.add_argument('server_address', help='host:port')
-parser.add_argument('der_dest_file_path')
+parser.add_argument('pem_dest_file_path')
 
 parser.add_argument('--server-name', default=None)
 
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     ssl_server_addr = host, int(port)
 
     with connect_ssl_insecure(ssl_server_addr, args.server_name) as sock:
-        with open(args.der_dest_file_path, 'w') as fio:
+        with open(args.pem_dest_file_path, 'w') as fio:
             der = sock.getpeercert(binary_form=True)
             fio.write(ssl.DER_cert_to_PEM_cert(der))
 
